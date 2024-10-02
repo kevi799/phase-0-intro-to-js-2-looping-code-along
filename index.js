5bdc2fd84099eff8799b977e96a6d8e209440f9a
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-02 08:51:58 +0300

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = [];
+//function that accepts two arguments an array of string names, and an event name
+function writeCards(array, name) {
+    for(let i = 0;i < array.length;i++) { 
+        const message = (`Thank you, ${array[i]}, for the wonderful ${name} gift!`)    
+        messages.push(message);
+    }
+    return messages;
+}
+//function that takes in any positive integer and counts down to zero
+function countDown(anyInt) {
+    while(anyInt >= 0) {
+        console.log(anyInt--);
+    }
+}
+countDown(10);
